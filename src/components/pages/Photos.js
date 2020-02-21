4d2c02b9e68dd39da6d7093b567c0f99eb35ab75
Pat <pat.cooney13@gmail.com>
2020-02-21 16:29:51 -0500

    delete post added

--- src/components/pages/Photos.js
+++ src/components/pages/Photos.js
@@ -1,5 +1,4 @@
-//add onclick handler for categories
-//click checks what category was clicked and runs through all posts for a match
+//Lazy loading
 
 //another handler for when image is clicekd changing it out from medium to large and zoom effect to look like a screen change
 
@@ -24,95 +23,96 @@ class Photos extends React.Component {
 
     componentDidMount() {
         const dataPosts =
-            'https://pat-cooney.com/wp/wp-json/wp/v2/photography?per_page=24&_embed';
+            'https://pat-cooney.com/wp/wp-json/wp/v2/photography?per_page=50&_embed';
 
-        fetch(dataPosts).then(value => value.json())
+        fetch(dataPosts)
+            .then(value => value.json())
             .then(value => {
-            value.sort(function(a, b) {
-                var textA = a.slug.toUpperCase();
-                var textB = b.slug.toUpperCase();
-                return textA < textB ? -1 : textA > textB ? 1 : 0;
-            });
-            this.setState(
-                {
-                    isLoaded: true,
-                    posts: value,
-                    allPosts: value,
-                },
-                error => {
-                    this.setState({
+                this.setState(
+                    {
                         isLoaded: true,
-                        error,
-                    });
-                }
-            );
-        });
+                        posts: value,
+                        allPosts: value,
+                    },
+                    error => {
+                        this.setState({
+                            isLoaded: true,
+                            error,
+                        });
+                    }
+                );
+            });
     }
 
     handleCategories(tag) {
-        console.log(tag.id)
-        // console.log(tag);
-        const categorizedPosts = [];
-        document
-            .querySelector('.secondary-header')
-            .classList.remove('show-secondary-nav');
-        this.state.allPosts.forEach(function(post) {
-            post.categories.forEach(function(categories) {
-                // console.log(categories);
-                if (categories === tag.id) {
-                    console.log("match");
-                    categorizedPosts.push(post);
-                }
-            });
-            // console.log(post.categories)
-        });
-        this.setState({
-            posts: categorizedPosts,
-        });
+        let categoryPosts = [];
+
+        this.state.allPosts.map(post =>
+            post.categories.map(
+                category =>
+                    //match post category to clicked category
+                    category === tag.id && categoryPosts.push(post)
+            )
+        );
+        this.setState(
+            {
+                isLoaded: true,
+                posts: categoryPosts,
+            },
+            error => {
+                this.setState({
+                    isLoaded: true,
+                    error,
+                });
+            }
+        );
     }
 
-    handleImage(image) {
-        let cardLinks = document.getElementsByClassName('card-image-link');
-        let showSecondaryNav = 0;
-        let secondaryNav = document.querySelector('.secondary-header');
+    handleImage(e, image) {
+        console.log(e.target);
+        if (e.target.className !== "delete-trash") {
+            let cardLinks = document.getElementsByClassName('card-image-link');
+            let showSecondaryNav = 0;
+            let secondaryNav = document.querySelector('.secondary-header');
 
-        for (var i = 0; i < cardLinks.length; i++) {
-            let card = cardLinks[i].parentElement.parentElement;
-            let cardImage = cardLinks[i].children[0];
-            //finds card that matchs url
-            if (
-                image.sizes.medium === cardLinks[i].children[0].src ||
-                image.url === cardLinks[i].children[0].src
-            ) {
-                //Put image title in secondary nav
-                showSecondaryNav++;
-                secondaryNav.children[0].children[0].innerHTML =
-                    card.children[1].children[0].innerHTML;
+            for (var i = 0; i < cardLinks.length; i++) {
+                let card = cardLinks[i].parentElement.parentElement;
+                let cardImage = cardLinks[i].children[0];
+                //finds card that matchs url
+                if (
+                    image.sizes.medium === cardLinks[i].children[0].src ||
+                    image.url === cardLinks[i].children[0].src
+                ) {
+                    //Put image title in secondary nav
+                    showSecondaryNav++;
+                    secondaryNav.children[0].children[0].innerHTML =
+                        card.children[1].children[0].innerHTML;
 
-                //changes image src to full-rez
-                if (cardImage.src === image.sizes.medium) {
-                    cardImage.src = image.url;
-                }
-                //Makes image full-width of the page
-                if (!card.classList.contains('clicked-full')) {
-                    card.classList.add('clicked-full');
-                    cardLinks[i].style.height = 'unset';
-                    cardImage.style.maxWidth = '100%';
-                    cardImage.style.width = '100%';
-                    cardImage.style.height = '100%';
+                    //changes image src to full-rez
+                    if (cardImage.src === image.sizes.medium) {
+                        cardImage.src = image.url;
+                    }
+                    //Makes image full-width of the page
+                    if (!card.classList.contains('clicked-full')) {
+                        card.classList.add('clicked-full');
+                        cardLinks[i].style.height = 'unset';
+                        cardImage.style.maxWidth = '100%';
+                        cardImage.style.width = '100%';
+                        cardImage.style.height = '100%';
+                    } else {
+                        card.classList.remove('clicked-full');
+                        cardImage.style.minWidth = 'unset';
+                        cardImage.style.maxWidth = 'unset';
+                    }
                 } else {
                     card.classList.remove('clicked-full');
-                    cardImage.style.minWidth = 'unset';
-                    cardImage.style.maxWidth = 'unset';
+                    console.log(this);
                 }
-            } else {
-                card.classList.remove('clicked-full');
-                console.log(this);
-            }
-            if (showSecondaryNav) {
-                secondaryNav.classList.add('show-secondary-nav');
-            } else {
-                secondaryNav.classList.remove('show-secondary-nav');
+                if (showSecondaryNav) {
+                    secondaryNav.classList.add('show-secondary-nav');
+                } else {
+                    secondaryNav.classList.remove('show-secondary-nav');
+                }
             }
         }
     }
@@ -120,7 +120,7 @@ class Photos extends React.Component {
     postshit() {
         console.log(this.state);
         console.log(Cookies.get());
-        let thePost = []
+        let thePost = [];
         // get post.token from above and paste into variable below
         fetch('https://pat-cooney.com/wp/wp-json/wp/v2/photography', {
             method: 'POST',
@@ -142,10 +142,35 @@ class Photos extends React.Component {
                 console.log(post);
                 thePost.push(post);
             });
-            this.setState({
-                allPosts: thePost[0],
-            })
-            console.log(this.state);
+        this.setState(
+            state => ({
+                isLoaded: true,
+                posts: state.posts + thePost,
+            }),
+            error => {
+                this.setState({
+                    isLoaded: true,
+                    error,
+                });
+            }
+        );
+    }
+
+    deleteshit(e, image) {
+        fetch(
+            `https://pat-cooney.com/wp/wp-json/wp/v2/photography/${image}`,
+            {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: 'application/json',
+                    Authorization: 'Bearer ' + Cookies.get('wp-auth-token'),
+                },
+            }
+        )
+        .catch(error => {
+            console.error(error);
+        });
     }
 
     clickedHeart(e, user) {
@@ -170,28 +195,31 @@ class Photos extends React.Component {
             return (
                 <div className="App">
                     <h1>Photos</h1>
-                    {console.log(this.state)}
-                    
+
+                    {this.props.username && (
                         <button onClick={() => this.postshit()}>
                             post shit
                         </button>
-                    
+                    )}
+                    {console.log(this.state.posts)}
+
                     <div className="card-container">
-                        {this.state.posts.map(post => (
+                        {this.state.posts.slice(0, 12).map(post => (
                             <div key={post.slug} className="card">
                                 <div className="card-imagebox">
                                     <Link
                                         to={`/photo/${post.slug}`}
                                         className="card-image-link"
-                                        onClick={() =>
-                                            this.handleImage(post.acf.image)
-                                        }>
+                                        onClick={(e) =>
+                                            this.handleImage(e, post.acf.image)
+                                        }
+                                        >
                                         <img
                                             src={
                                                 post.acf.image
                                                     ? post.acf.image.sizes
                                                           .medium
-                                                    : 'https:via.placeholder.com/300x350/126b4c/ffffff'
+                                                    : 'https:via.placeholder.com/300x350/9aaca0/ffffff'
                                             }
                                             className="card-image"
                                             alt={post.title.rendered
@@ -202,11 +230,20 @@ class Photos extends React.Component {
                                                 className="author-image"
                                                 style={{
                                                     backgroundImage: `url(${post._embedded.author[0]['avatar_urls']['24']})`,
-                                                }}
-                                                ></div>
+                                                }}></div>
                                             <p className="author">
                                                 {post._embedded.author[0].name}
                                             </p>
+                                            {post._embedded.author[0].slug ===
+                                                Cookies.get('username') && (
+                                                <button
+                                                    className="delete-trash"
+                                                    onClick={(e) =>
+                                                        this.deleteshit(e, post.id)
+                                                    }>
+                                                    &#128465;
+                                                </button>
+                                            )}
                                         </div>
                                     </Link>
                                 </div>
@@ -224,7 +261,7 @@ class Photos extends React.Component {
                                         <div className="heart-box">
                                             <input
                                                 id={`hrt-${post.slug}`}
-                                                class="heart-input"
+                                                className="heart-input"
                                                 type="checkbox"
                                             />
                                             <label
@@ -240,35 +277,38 @@ class Photos extends React.Component {
                                         </div>
                                     </div>
                                     <p className="card-categories">
-                                        {post._embedded['wp:term'] ?
-                                            post._embedded['wp:term'][0].map(tag => (
-                                                <Link
-                                                    key={counter++}
-                                                    className="card-category"
-                                                    style={{
-                                                        backgroundColor: tag.acf.color,
-                                                        borderTopColor: tag.acf.color,
-                                                    }}
-                                                    to={`/photos/${tag.slug}`}
-                                                    onClick={() =>
-                                                        this.handleCategories(tag)
-                                                    }
-                                                    data-tooltip={
-                                                        tag.name
-                                                            .charAt(0)
-                                                            .toUpperCase() +
-                                                        tag.name.slice(1)
-                                                    }
-                                                    aria-hidden="true"
-                                                >
-                                                    {tag.name
-                                                        .slice(0, 1)
-                                                        .toUpperCase()}
-                                                </Link>
-                                        ))
-                                        :
-                                        <p></p>
-                                    }
+                                        {post._embedded['wp:term']
+                                            ? post._embedded['wp:term'][0].map(
+                                                  tag => (
+                                                      <Link
+                                                          key={counter++}
+                                                          className="card-category"
+                                                          style={{
+                                                              backgroundColor:
+                                                                  tag.acf.color,
+                                                              borderTopColor:
+                                                                  tag.acf.color,
+                                                          }}
+                                                          to={`/photos/${tag.slug}`}
+                                                          onClick={() =>
+                                                              this.handleCategories(
+                                                                  tag
+                                                              )
+                                                          }
+                                                          data-tooltip={
+                                                              tag.name
+                                                                  .charAt(0)
+                                                                  .toUpperCase() +
+                                                              tag.name.slice(1)
+                                                          }
+                                                          aria-hidden="true">
+                                                          {tag.name
+                                                              .slice(0, 1)
+                                                              .toUpperCase()}
+                                                      </Link>
+                                                  )
+                                              )
+                                            : ''}
                                     </p>
                                 </div>
                             </div>
